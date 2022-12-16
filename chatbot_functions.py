f692471aad45dc555f6a294868911343ce2cdb6a
Mun-Min <minhasmunqiz@gmail.com>
2022-12-15 17:40:59 -0700

    updated chatbot UI & chatbot functions to include portfolio aggregation

--- chatbot_functions.py
+++ chatbot_functions.py
@@ -1,4 +1,9 @@
+###############################################################################################################
+#      This python file includes the neccesary functions that allow the chatbot's aggregations to run         #          
+###############################################################################################################
+
 from streamlit_chat import message
+import streamlit as st
 
 def verifyUserAge(age): 
     '''
@@ -18,3 +23,47 @@ def verifyUserAge(age):
 
         # Add a message to the chat widget indicating the user is under 18 years old or the input is not a valid age
         message("This application requires you to be at least 18 years old.", seed=21)
+
+portfolio_list = []
+weights_list = []
+user_buying_power_allocation = []
+
+#@st.cache(max_entries=1, allow_output_mutation=True) 
+def determine_weights(age): 
+    '''
+    This function calculates the weights for the chosen portfolio based off of the 
+    user's age and a traditional investment strategy called the 110 rule 
+    
+    Parameters: 
+
+    age --> pass in the user's age from streamlit to be calculated through the 110 rule
+    portfolio_type --> pass in the chosen portfolio_type from streamlit to ensure weights are entered correctly
+    '''
+    weights_list.clear()
+
+    user_stock_weights = 110 - age
+    user_bonds_crypto_weights = (100 - user_stock_weights) / 2
+
+    user_bonds_crypto_weights = user_bonds_crypto_weights / 100
+    user_stock_weights = user_stock_weights / 100
+    
+    weights_list.append(user_stock_weights)
+    weights_list.append(user_bonds_crypto_weights)
+    weights_list.append(user_bonds_crypto_weights)
+    
+def allocate_portfolio(user_investment_amount):
+    '''
+    This function allocates the user's buying power towards each asset class based on the weights calculated above
+    
+    Parameters: 
+
+    user_investment_amount --> pass in the user_investment_amount from streamlit to calculate how much money the user will invest in each asset class
+    '''
+    user_buying_power_allocation.clear()
+    
+    for weight in weights_list: 
+        investments_per_asset = float(user_investment_amount) * float(weight)
+        user_buying_power_allocation.append('$' + str(investments_per_asset))
+    
+    message(f'Amount of buying power allocated towards each asset class (Stocks/Bonds/Crypto): ', seed=21, key=10)
+    message(f'{user_buying_power_allocation}', seed=21, key=11)
--- test_chatbot_UI.py
+++ test_chatbot_UI.py
@@ -1,17 +1,19 @@
 ###############################################################################################################
-# Developing a chatbot that will generate a weighted investment portfolio based on the user's risk tolerance  #                                                                                                            #
+# Developing a chatbot that will generate a weighted investment portfolio based on the user's risk tolerance  # 
 # and age, utilizing Streamlit, Python, Machine Learning, & Traditional Financial Metrics.                    #
 ###############################################################################################################
 
 import streamlit as st
 from streamlit_chat import message
 from chatbot_functions import verifyUserAge
+from chatbot_functions import determine_weights
+from chatbot_functions import allocate_portfolio
 
 st.markdown("## Investment Portfolio Generator")
 
 # Use the message function to create a chat-like UI
 message("Hello, I'm a chatbot that can generate a weighted investment portfolio for you!", seed=21)
-message("Please enter your age in the sidebar!", seed=21)
+message("Please enter your age", seed=21)
 
 # Ask the user for their age
 user_age = st.sidebar.text_input("Enter your age: ")
@@ -27,13 +29,49 @@ try:
 
         # verify the user's age 
         verifyUserAge(age)
-    
+
+        if age >= 18: 
+
+            # Ask the user for their desired investment amount 
+            message("Please enter your desired investment amount in USD", seed=21, key=2)
+            user_investment_amount = st.sidebar.text_input("Enter investment amount in USD: ", key=3)
+            message(user_investment_amount, is_user=True, seed=1, key=4)
+            
+            # Ask the user for their desired portfolio type (risk tolerance)
+            if user_investment_amount: 
+                if user_investment_amount.isnumeric():
+                    message("Please enter a portfolio type that matches your risk tolerance (High Risk Portfolio, Low Risk Portfolio, Moderate Risk Portfolio)", seed=21, key=5)
+                    portfolio_type = st.sidebar.text_input('Choose one of the following: ')
+
+                    valid_portfolio_types = ['high risk portfolio', 'low risk portfolio', 'moderate risk portfolio', 'high risk portfolio ', 'low risk portfolio ', 'moderate risk portfolio ']
+
+                    if portfolio_type: 
+                        if str(portfolio_type).lower() in valid_portfolio_types: 
+                            if str(portfolio_type).lower() == 'high risk portfolio' or str(portfolio_type).lower() == 'high risk portfolio ': 
+
+                                # generate a High Risk Portfolio based on expected return vs risk (MPT)
+                                # stocks --> TSLA, NVDA 
+                                # bonds --> Treasury Yield 10yr 
+                                # crypto --> ETH
+
+                                portfolio_list = ['TSLA', 'NVDA', '10yr Treasury Yield', 'ETH']
+                                message('Your High-Risk Portfolio contains the following assets: ', seed=21, key=6)
+                                message(f'{portfolio_list}', seed=21, key=7)
+
+                                # calculate weights for portfolio
+                                determine_weights(age)
+                                allocate_portfolio(user_investment_amount)
+                        else: 
+                            message("I'm sorry, but it looks like you entered an invalid portfolio type. Please enter a valid portfolio type!", seed=21)
+                else:
+                    message("I'm sorry, but it looks like you entered an invalid number. Please enter a valid number!", seed=21)
     else:
         # The user hasn't entered a response yet, so don't show any messages in the chat widget
         pass
 
 except ValueError:
-    message("I'm sorry, but it looks like you entered an invalid number for your age. Please enter a valid number!", seed=21)
+    message("I'm sorry, but it looks like you entered an invalid number. Please enter a valid number!", seed=21)
+
 
 # TO:DO --> grab user's investment amount, 
 #           calculate weighted portfolio based off of 110 rule, 
